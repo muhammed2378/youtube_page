629fcac34858595c61d3e5e793d228a3ef3348c3
muhammed2378 <salnhmohammed123@gmail.com>
2023-03-05 00:35:29 +0300

    added all settigns-box with events handling and it's responsive

--- js/main.js
+++ js/main.js
@@ -51,3 +51,69 @@ backIcon.onclick = function () {
     searchEngine.classList.remove("active");
 }
 
+/* the settings-box and onclicking the icon bar */
+
+// the bar icon in the heading
+let barIcon = document.querySelector(".heading .bar-icon");
+
+
+// the selectors container
+let contentSelector = document.querySelector(".content-selectors");
+
+// the settings box
+let settingsBox = document.querySelector(".settings-box")
+
+// the divs inside the settings box
+let settingsBoxOpt = document.querySelector(".settings-box div")
+
+// the bar icon in the settings box
+let iconBarBox = document.querySelector(".settings-box .logo-bar .bar-icon");
+
+// the previous icon
+let previousIcon = document.querySelector(".previous");
+
+
+// create layer 
+let layer = document.createElement("div");
+document.body.append(layer)
+
+
+barIcon.onclick = () => {
+    // only if it's greater than 1320 screen width
+    if(window.innerWidth > 1320) {
+        // remove the small class from the content selectors and the settings box
+        contentSelector.classList.toggle("small")
+        previousIcon.classList.toggle("small")
+        settingsBox.classList.toggle("small")
+    } else {
+        // if it's not greater than 1320 screen width
+
+        // add a layer above all
+        layer.classList.add("overlay");
+
+        // remove the 'all' class to remove the old style
+        settingsBox.classList.remove("all")
+
+        // add the 'box' class to add the new style of the box
+        settingsBox.classList.add("box")
+
+        // remove the 'small' class if it's exsit previously
+        settingsBox.classList.remove("small")
+    }
+    
+}
+
+
+// on clicking the inside bar icon
+
+iconBarBox.onclick = () => {
+
+    // remove the overlay
+    layer.classList.remove("overlay");
+
+    // add the "all" class to add the normal style
+    settingsBox.classList.add("all")
+
+    // remove the 'box' class to remove the box
+    settingsBox.classList.remove("box")
+}
